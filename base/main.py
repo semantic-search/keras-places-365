88f8c18abf1a76510cecbe71d769462f870516bd
jainal09 <jainal09gosalia@gmail.com>
2020-08-22 02:45:52 +0530

    all codes added

--- base/main.py
+++ base/main.py
@@ -3,11 +3,11 @@ from urllib.request import urlopen
 import numpy as np
 from PIL import Image
 from cv2 import resize
-from vgg16_hybrid_places_1365 import VGG16_Hybrid_1365
+from vgg16_places_365 import VGG16_Places365
 
-LABELS_URL = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_hybrid1365.txt'
+LABELS_URL = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_places365.txt'
 LABELS = np.array(urlopen(LABELS_URL).read().splitlines())
-model = VGG16_Hybrid_1365()
+model = VGG16_Places365()
 
 
 app = FastAPI()
@@ -25,19 +25,15 @@ def create_upload_file(file: UploadFile = File(...)):
     image = np.expand_dims(image, 0)
     output = model.predict(image)
     output = np.squeeze(output)
-    labels = []
-    scores = []
     new_labels = []
     top5 = output.argsort()[-5:][::-1]
     labels = LABELS[top5]
     scores = output[top5]
     for vals in labels:
         decoded_string_array = vals.decode('UTF-8')
-        string_array_without_quotes = decoded_string_array.replace(',', '')
-        array_without_quotes = string_array_without_quotes.split(" ")
-        array_without_quotes.pop()
-        array_without_quotes.pop(0)
-        new_labels.append(array_without_quotes)
+        array_with_id = decoded_string_array.split(" ")
+        array_with_id.pop()
+        new_labels.append(array_with_id[0])
 
     scores = [float(np_float) for np_float in scores]
     response_dict = {
