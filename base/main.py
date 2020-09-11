9444d9d1554f5f44b22cf2103a687c3d4fc02bce
Ubuntu <jainal09@research-paper-2.rdgzozfc15lepcxsiapnqsrqpg.bx.internal.cloudapp.net>
2020-09-11 10:50:23 +0000

    added code for base

--- base/main.py
+++ base/main.py
@@ -1,100 +1,54 @@
-from urllib.request import urlopen
-import numpy as np
-from PIL import Image
-from cv2 import resize
-from vgg16_places_365 import VGG16_Places365
-import redis
-from kafka import KafkaConsumer
-from kafka import KafkaProducer
-from json import loads
-import base64
-import os
-import uuid
+
 import json
-from dotenv import load_dotenv
-from base64 import decodestring
-
-load_dotenv()
-KAFKA_HOSTNAME = os.getenv("KAFKA_HOSTNAME")
-KAFKA_PORT = os.getenv("KAFKA_PORT")
-REDIS_HOSTNAME = os.getenv("REDIS_HOSTNAME")
-REDIS_PORT = os.getenv("REDIS_PORT")
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-# kafka prerequisites
-RECEIVE_TOPIC = 'KERAS_BASE'
-SEND_TOPIC_FULL = "IMAGE_RESULTS"
-SEND_TOPIC_TEXT = "TEXT"
-print("kafka : " + KAFKA_HOSTNAME + ':' + KAFKA_PORT)
-
-LABELS_URL = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_places365.txt'
-LABELS = np.array(urlopen(LABELS_URL).read().splitlines())
-model = VGG16_Places365()
-# Redis initialize
-r = redis.StrictRedis(host=REDIS_HOSTNAME, port=REDIS_PORT,
-                      password=REDIS_PASSWORD, ssl=True)
-consumer_easyocr = KafkaConsumer(
-    RECEIVE_TOPIC,
-    bootstrap_servers=[KAFKA_HOSTNAME + ':' + KAFKA_PORT],
-    auto_offset_reset="earliest",
-    enable_auto_commit=True,
-    group_id="my-group",
-    value_deserializer=lambda x: loads(x.decode("utf-8")),
-)
-producer = KafkaProducer(
-    bootstrap_servers=[KAFKA_HOSTNAME + ':' + KAFKA_PORT],
-    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-)
+import uuid
+from db_models.mongo_setup import global_init
+from db_models.models.cache_model import Cache
+import init
+from places_base import predict
+import globals
 
 
-def predict(file_name,image_id):
-    image = Image.open(file_name)
-    image = np.array(image, dtype=np.uint8)
-    image = resize(image, (224, 224))
-    # image = preprocess_input(image.astype(np.float32))
-    image = np.expand_dims(image, 0)
-    output = model.predict(image)
-    output = np.squeeze(output)
-    new_labels = []
-    top5 = output.argsort()[-5:][::-1]
-    labels = LABELS[top5]
-    scores = output[top5]
-    for vals in labels:
-        decoded_string_array = vals.decode('UTF-8')
-        array_with_id = decoded_string_array.split(" ")
-        array_with_id.pop()
-        new_labels.append(array_with_id[0])
-
-    scores = [float(np_float) for np_float in scores]
-    response_dict = {
-            "labels": new_labels,
-            "scores": scores,
-            "image_id": image_id,
-
-    }
-    return response_dict
+def send_to_topic(topic, value_to_send_dic):
+    data_json = json.dumps(value_to_send_dic)
+    init.producer_obj.send(topic, value=data_json)
 
 if __name__ == "__main__":
-    print("shit jere")
-    for message in consumer_easyocr:
-        print('xxx--- inside open images consumer---xxx')
-        print(KAFKA_HOSTNAME + ':' + KAFKA_PORT)
-
+    for message in init.consumer_obj:
+        global_init()
         message = message.value
-        print("MESSAGE RECEIVED consumer_densecap: ")
-        image_id = message['image_id']
-        # data = message['data']
-        data = message['data']
-        r.set(RECEIVE_TOPIC, image_id)
-        file_name = str(uuid.uuid4()) + ".jpg"
-        with open(file_name, "wb") as fh:
-            fh.write(base64.b64decode(data.encode("ascii")))
+        db_key = str(message)
+        db_object = Cache.objects.get(pk=db_key)
+        file_name = db_object.file_name
+        init.redis_obj.set(globals.RECEIVE_TOPIC, file_name)
+        if db_object.is_doc_type:
+            """document"""
+            images_array = []
+            for image in db_object.files:
+                pdf_image = str(uuid.uuid4()) + ".jpg"
+                with open(pdf_image, 'wb') as file_to_save:
+                    file_to_save.write(image.file.read())
+                images_array.append(pdf_image)
+            full_results = []
+            for image in images_array:
+                image_results = predict(image, doc=True)
+                full_results.append(image_results)
+            final_full_response = {
+                "container_name": globals.RECEIVE_TOPIC,
+                "file_name": file_name,
+                "results": full_results,
+                "is_doc_type": True
+            }
+            print(final_full_response)
+            send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=final_full_response)
+            init.producer_obj.flush()
+        else:
+            """image"""
+            if db_object.mime_type in globals.ALLOWED_IMAGE_TYPES:
+                with open(file_name, 'wb') as file_to_save:
+                    file_to_save.write(db_object.file.read())
+                image_results = predict(file_name)
+                image_results["container_name"] = globals.RECEIVE_TOPIC
+                print(image_results)
+                send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=image_results)
+                init.producer_obj.flush()
 
-        full_res = predict(file_name, image_id)
-        text_res = {
-            "image_id": full_res["image_id"],
-            "captions": full_res["labels"]
-        }
-        producer.send(SEND_TOPIC_FULL, value=json.dumps(full_res))
-        producer.send(SEND_TOPIC_TEXT, value=json.dumps(text_res))
-
-        producer.flush()
---
+++ base/places_base.py
@@ -0,0 +1,49 @@
+import os
+from urllib.request import urlopen
+import numpy as np
+from PIL import Image
+from cv2 import resize
+from vgg16_places_365 import VGG16_Places365
+LABELS_URL = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_places365.txt'
+LABELS = np.array(urlopen(LABELS_URL).read().splitlines())
+model = VGG16_Places365()
+# Redis initialize
+
+
+def predict(file_name, doc=False):
+    image = Image.open(file_name)
+    image = np.array(image, dtype=np.uint8)
+    image = resize(image, (224, 224))
+    # image = preprocess_input(image.astype(np.float32))
+    image = np.expand_dims(image, 0)
+    output = model.predict(image)
+    output = np.squeeze(output)
+    new_labels = []
+    top5 = output.argsort()[-5:][::-1]
+    labels = LABELS[top5]
+    scores = output[top5]
+    for vals in labels:
+        decoded_string_array = vals.decode('UTF-8')
+        array_with_id = decoded_string_array.split(" ")
+        array_with_id.pop()
+        new_labels.append(array_with_id[0])
+
+    scores = [float(np_float) for np_float in scores]
+
+    if doc:
+        response_dict = {
+            "objects": new_labels,
+            "score": scores
+        }
+        os.remove(file_name)
+        return response_dict
+    else:
+        response_dict = {
+            "file_name": file_name,
+            "objects": new_labels,
+            "score": scores,
+            "is_doc_type": False
+        }
+        os.remove(file_name)
+        return response_dict
+
